e136c59433aecc8a12f62782d98373f2cf79fd4b
SwapnilSkr <swapnilmkab@gmail.com>
2026-02-17 15:12:55 +0530

    security measures

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants {
   static String get apiBaseUrl =>
-      dotenv.env['API_BASE_URL'] ?? 'https://06f5-14-97-183-132.ngrok-free.app';
+      dotenv.env['API_BASE_URL'] ?? 'http://10.0.2.2:3000';
 
   static String? get serverClientId => dotenv.env['GOOGLE_SERVER_CLIENT_ID'];
 }
